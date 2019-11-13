f820de468d15505dbcba834588c957e3d3e4c28b
Agile <agile.8118@usa.com>
2019-11-13 22:01:25 +0330

    Improve the app for CI

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,7 +1,8 @@
-if (process.env.NODE_ENV === "production") {
+if (
+  process.env.NODE_ENV === "production" ||
+  (process.env.NODE_ENV === "test" && process.env.DOMAIN)
+) {
   module.exports = require("./prod");
-} else if (process.env.NODE_ENV === "test") {
-  module.exports = require("./testing");
 } else {
   module.exports = require("./dev");
 }
--- server/config/prod.js
+++ server/config/prod.js
@@ -1,3 +1,11 @@
 module.exports = {
-  domain: process.env.DOMAIN
+  domain: process.env.DOMAIN,
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  cookieKey: process.env.COOKIE_KEY,
+  mysqlHost: process.env.MYSQL_HOST,
+  mysqlUser: process.env.MYSQL_USER,
+  mysqlDatabase: process.env.MYSQL_DATABASE,
+  mysqlPassword: process.env.MYSQL_PASSWORD,
+  mysqlPort: process.env.MYSQL_PORT
 };
