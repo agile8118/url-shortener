b19554874a2b3ee7bf5c0b573c4ba22bbca6e7ca
Agile <agile.8118@usa.com>
2020-08-03 18:17:32 +0430

    Fixes a bug with start script

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,11 +2,8 @@
 export PATH="$PATH:/home/ec2-user/.nvm/versions/node/v12.18.3/bin:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/home/ec2-user/.local/bin:/home/ec2-user/bin"
 
 echo Installing the dependencies
-cd cd /home/ec2-user/url-shortener/
+cd /home/ec2-user/url-shortener/
 ~/.nvm/versions/node/v12.18.3/bin/npm install
 
 echo Starting the app
 ~/.nvm/versions/node/v12.18.3/bin/pm2 start /home/ec2-user/url-shortener/ecosystem.config.js
-
-echo Nginx conf status
-sudo /sbin/nginx -t
