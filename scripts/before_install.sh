96ffab0e2972912d1139e43c140d7757001bdba7
Agile <agile.8118@usa.com>
2020-08-03 18:11:38 +0430

    Updates the path variable before running the scripts

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,7 +2,6 @@
 # sudo rm -rf /home/ec2-user/url-shortener/app
 whoami
 pwd
-sudo /sbin/nginx -t
 # echo About to stop pm2 app
 # /home/ec2-user/.nvm/versions/node/v12.18.3/bin/pm2 stop url_shortener
 # echo About to delete pm2 app
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
-whoami
-pwd
-echo $PATH
 export PATH="$PATH:/home/ec2-user/.nvm/versions/node/v12.18.3/bin:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/home/ec2-user/.local/bin:/home/ec2-user/bin"
-echo $PATH
-echo Node.js Version:
-~/.nvm/versions/node/v12.18.3/bin/node -v
-echo PM2 Version:
-~/.nvm/versions/node/v12.18.3/bin/pm2 -v
-echo npm Version:
-~/.nvm/versions/node/v12.18.3/bin/npm  -v
-# cd /home/ec2-user/url-shortener/
-# /home/ec2-user/.nvm/versions/node/v12.18.3/bin/npm install
-# /home/ec2-user/.nvm/versions/node/v12.18.3/bin/pm2 start /home/ec2-user/url-shortener/ecosystem.config.js
+
+echo Installing the dependencies
+cd cd /home/ec2-user/url-shortener/
+~/.nvm/versions/node/v12.18.3/bin/npm install
+
+echo Starting the app
+~/.nvm/versions/node/v12.18.3/bin/pm2 start /home/ec2-user/url-shortener/ecosystem.config.js
+
+echo Nginx conf status
+sudo /sbin/nginx -t
