f665796918da67bbe15a19422e46b6ee7138e231
Agile <agile.8118@usa.com>
2020-08-03 16:07:33 +0430

    Kills pm2 app before updating to the new version

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 # sudo rm -rf /home/ec2-user/url-shortener/app
+echo About to stop pm2 app
+/home/ec2-user/.nvm/versions/node/v12.18.3/bin/pm2 stop url_shortener
+echo About to delete pm2 app
+/home/ec2-user/.nvm/versions/node/v12.18.3/bin/pm2 delete url_shortener
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-cd /home/ec2-user/url-shortener/
-touch test.txt
-touch /home/ec2-user/url-shortener/test2.txt
+# cd /home/ec2-user/url-shortener/
 # /home/ec2-user/.nvm/versions/node/v12.18.3/bin/npm install
 # /home/ec2-user/.nvm/versions/node/v12.18.3/bin/pm2 start /home/ec2-user/url-shortener/ecosystem.config.js
