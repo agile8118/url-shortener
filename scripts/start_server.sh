99d615815b78f2bd3d00cd89eccdd6222d91e2d3
Agile <agile.8118@usa.com>
2020-08-03 15:47:49 +0430

    Updates CodeDeploy conf for debugging

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 cd /home/ec2-user/url-shortener/
-/home/ec2-user//.nvm/versions/node/v12.18.3/bin/npm install
-/home/ec2-user/.nvm/versions/node/v12.18.3/bin/pm2 start /home/ec2-user/url-shortener/ecosystem.config.js
+touch test.txt
+touch /home/ec2-user/url-shortener/test2.txt
+# /home/ec2-user/.nvm/versions/node/v12.18.3/bin/npm install
+# /home/ec2-user/.nvm/versions/node/v12.18.3/bin/pm2 start /home/ec2-user/url-shortener/ecosystem.config.js
