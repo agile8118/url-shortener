3308e1b18c03bc2bef6bc9e5791958e5d3d667ba
Agile <agile.8118@usa.com>
2020-08-03 16:28:44 +0430

    Updates script files for debugging

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # sudo rm -rf /home/ec2-user/url-shortener/app
-echo About to stop pm2 app
-/home/ec2-user/.nvm/versions/node/v12.18.3/bin/pm2 stop url_shortener
-echo About to delete pm2 app
-/home/ec2-user/.nvm/versions/node/v12.18.3/bin/pm2 delete url_shortener
+whoami
+pwd
+sudo /sbin/nginx -t
+# echo About to stop pm2 app
+# /home/ec2-user/.nvm/versions/node/v12.18.3/bin/pm2 stop url_shortener
+# echo About to delete pm2 app
+# /home/ec2-user/.nvm/versions/node/v12.18.3/bin/pm2 delete url_shortener
--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+whoami
+pwd
 # echo About to stop pm2 app
 # /home/ec2-user/.nvm/versions/node/v12.18.3/bin/pm2 stop url_shortener
 # echo About to delete pm2 app
